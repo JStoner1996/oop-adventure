ee37e44ff106b49c599b346edc9ca74c999ec8bf
Jake Stoner <jakestoner123@live.com>
2026-01-05 17:03:33 +0000

    --feat: Initial set up and class creation

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using OOPAdventure;
+
+Console.WriteLine("Hello, What is your name?");
+
+var name = Console.ReadLine();
+
+if (name == string.Empty)
+{
+    name = "No name.";
+}
+
+var player = new Player(name);
+
+Console.WriteLine("Welcome {0}, to your OOP Adventure.", player.Name);
